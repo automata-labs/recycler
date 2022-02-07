6550c9c2f0e07cfa0a2a31a11fd4998c5da7904e
Tony Jin <nijynot@gmail.com>
2022-02-07 12:14:50 +0100

    test: add comments and more tests

--- src/test/Recycler.t.sol
+++ src/test/Recycler.t.sol
@@ -3,14 +3,33 @@ pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
 
+import "../interfaces/IRecycler.sol";
 import "../libraries/SafeTransfer.sol";
 import "../Recycler.sol";
+import "../RecyclerManager.sol";
 import "./utils/Utilities.sol";
+import "./utils/Vm.sol";
+
+contract User is Vm, Utilities {
+    RecyclerManager public manager;
+
+    constructor(RecyclerManager manager_) {
+        manager = manager_;
+    }
+
+    function mint(uint256 amount) public {
+        startPrank(address(this));
+        mint(address(this), amount);
+        tokeVotePool.approve(address(manager), type(uint256).max);
+        manager.mint(address(this), amount);
+        stopPrank();
+    }
+}
 
 contract RecyclerTest is DSTest, Vm, Utilities {
     Recycler public recycler;
+    RecyclerManager public manager;
 
-    Manager public manager;
     User public user0;
     User public user1;
     User public user2;
@@ -28,7 +47,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
 
     function setUp() public {
         recycler = new Recycler(address(tokeVotePool), 0);
-        manager = new Manager(recycler, address(tokeVotePool));
+        manager = new RecyclerManager(address(tokeVotePool), IRecycler(recycler));
         user0 = new User(manager);
         user1 = new User(manager);
         user2 = new User(manager);
@@ -142,9 +161,20 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.epochAs(2).filled, false);
     }
 
+    // try creating an epoch with past timestamp as deadline
     function testNextPast() public {
+        recycler.next(1337);
+        assertEq(recycler.cursor(), 1);
+        assertEq(recycler.epochAs(1337).deadline, 0);
+        assertEq(recycler.epochAs(1).amount, 0);
+        assertEq(recycler.epochAs(1).shares, 0);
+        assertEq(recycler.epochAs(1).filled, false);
+
+        // make sure that it can be filled, otherwise soft-locked
+        recycler.fill(1);
     }
 
+    // try creating an epoch with zero timestamp as deadline
     function testNextZero() public {
         recycler.next(0);
         assertEq(recycler.cursor(), 1);
@@ -152,6 +182,9 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.epochAs(1).amount, 0);
         assertEq(recycler.epochAs(1).shares, 0);
         assertEq(recycler.epochAs(1).filled, false);
+
+        // make sure that it can be filled, otherwise soft-locked
+        recycler.fill(1);
     }
 
     function testNextUnauthorizedError() public {
@@ -189,6 +222,8 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.balanceOf(address(this)), 2e18);
     }
 
+    // should mint multiple times on the same epoch
+    // shouldn't make a difference if it's many small or one big tx
     function testMintMultipleDepositsOnSameEpoch() public {
         recycler.next(uint32(block.timestamp + 1));
 
@@ -201,6 +236,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.balanceOf(address(user0)), 3e18);
     }
 
+    // should tick the buffer and then mint afterwards
     function testMintTickBufferIfFilled() public {
         recycler.next(uint32(block.timestamp + 1));
 
@@ -249,6 +285,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.balanceOf(address(user0)), 3e18 + 1);
     }
 
+    // throw if admin has paused mint
     function testMintPausedError() public {
         mint(address(this), 1e18);
         tokeVotePool.approve(address(manager), type(uint256).max);
@@ -258,6 +295,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         manager.mint(address(this), 1e18);
     }
 
+    // throw when calling when only init:ed
     function testMintOnInitializedContractError() public {
         mint(address(this), 1e18);
         tokeVotePool.approve(address(manager), type(uint256).max);
@@ -265,6 +303,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         manager.mint(address(this), 1e18);
     }
 
+    // throw when not enough coins were transferred
     function testMintInsufficientTransferError() public {
         recycler.next(_deadline(100));
 
@@ -273,6 +312,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         manager.mint(address(this), 1e18);
     }
 
+    // throw when deadline is in the past, but we've deposited to it in the past
     function testMintEpochExpiredWhenFilledError() public {
         recycler.next(_deadline(1));
         recycler.fill(1);
@@ -283,6 +323,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         manager.mint(address(this), 1e18);
     }
 
+    // throw when deadline is in the past
     function testMintEpochExpiredWhenDeadlinePassedError() public {
         recycler.next(_deadline(100));
         // timetravel to when deadline is hit
@@ -294,6 +335,11 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         manager.mint(address(this), 1e18);
     }
 
+    // throw when a user has deposited into a past epoch, and that epoch has not been filled.
+    // if the user's buffer was empty, then it wouldn't matter when they deposited - all that'd be
+    // needed would be that the current epoch is available for deposits.
+    //
+    // to prevent the error, the admin needs to the fill the previous epochs.
     function testMintPastBufferExistsButNotFilledError() public {
         recycler.next(_deadline(1));
         // deposit
@@ -327,6 +373,7 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         stopPrank();
     }
 
+    // burn when the owner has approved enough to the spender/burner
     function testBurnUsingAllowance() public {
         recycler.next(_deadline(1));
         user0.mint(1e18);
@@ -341,6 +388,8 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(tokeVotePool.balanceOf(address(user1)), 1e18);
     }
 
+    // throw if trying to burn another users shares without permissions
+    // i.e. allowance underflows
     function testBurnWhenNotAllowedError() public {
         recycler.next(_deadline(1));
         user0.mint(1e18);
@@ -385,11 +434,34 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.epochAs(1).filled, true); // +
     }
 
+    // fill should never fail, even if deposited coins was zero
+    // try filling multiple in a row
+    function testFillZeroShares() public {
+        recycler.next(uint32(block.timestamp + 1));
+        recycler.fill(1);
+
+        recycler.next(uint32(block.timestamp + 1));
+        recycler.fill(2);
+
+        recycler.next(uint32(block.timestamp + 1));
+        recycler.fill(3);
+
+        assertEq(recycler.epochAs(1).shares, 0); // +
+        assertEq(recycler.epochAs(1).filled, true); // +
+        assertEq(recycler.epochAs(2).shares, 0); // +
+        assertEq(recycler.epochAs(2).filled, true); // +
+        assertEq(recycler.epochAs(3).shares, 0); // +
+        assertEq(recycler.epochAs(3).filled, true); // +
+    }
+
+    function testFillDiscontinousError() public {
+    }
+
     /**
-     * `tick`
+     * `poke` (or `tick`, which is its internal name)
      */
 
-    function testTick() public {
+    function testPoke() public {
         recycler.next(uint32(block.timestamp + 1));
         user0.mint(1e18);
 
@@ -402,14 +474,31 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.bufferAs(address(user0)).amount, 0);
     }
 
-    function testTickOnEmptyBuffer() public {
+    function testPokeOnEmptyBuffer() public {
+        recycler.poke(address(this));
+        assertEq(recycler.balanceOf(address(this)), 0);
     }
 
-    function testTickOnAlreadyTickedBuffer() public {
+    function testPokeOnAlreadyTickedBuffer() public {
+        recycler.next(uint32(block.timestamp + 1));
+        user0.mint(1e18);
+
+        // fill and tick, and then check holdings
+        recycler.fill(1);
+        recycler.poke(address(user0));
+        assertEq(recycler.sharesOf(address(user0)), 1e18);
+        assertEq(recycler.bufferAs(address(user0)).epoch, 0);
+        assertEq(recycler.bufferAs(address(user0)).amount, 0);
+
+        // nothing should change
+        recycler.poke(address(user0));
+        assertEq(recycler.sharesOf(address(user0)), 1e18);
+        assertEq(recycler.bufferAs(address(user0)).epoch, 0);
+        assertEq(recycler.bufferAs(address(user0)).amount, 0);
     }
 
     /**
-     * Integration tests
+     * Integration
      */
 
     function testMultipleUsers() public {
@@ -497,57 +586,5 @@ contract RecyclerTest is DSTest, Vm, Utilities {
         assertEq(recycler.sharesOf(address(user0)), 1e18 + 5e17);
         assertEq(recycler.bufferAs(address(user0)).epoch, 0);
         assertEq(recycler.bufferAs(address(user0)).amount, 0);
-    }
-}
-
-contract User is Vm, Utilities {
-    Manager public manager;
-
-    constructor(Manager manager_) {
-        manager = manager_;
-    }
-
-    function mint(uint256 amount) public {
-        startPrank(address(this));
-        mint(address(this), amount);
-        tokeVotePool.approve(address(manager), type(uint256).max);
-        manager.mint(address(this), amount);
-        stopPrank();
-    }
-}
-
-contract Manager {
-    using SafeTransfer for address;
-
-    struct CallbackData {
-        address token;
-        address payer;
-        address payee;
-        uint256 amount;
-    }
-
-    Recycler public recycler;
-    address public token;
-
-    constructor(Recycler recycler_, address token_) {
-        recycler = recycler_;
-        token = token_;
-    }
-
-    function mint(address to, uint256 amount) public {
-        CallbackData memory data = CallbackData({
-            token: token,
-            payer: msg.sender,
-            payee: address(recycler),
-            amount: amount
-        });
-
-        recycler.mint(to, amount, abi.encode(data));
-    }
-
-    function mintCallback(bytes memory data) public {
-        require(msg.sender == address(recycler), "Manager: Unauthorized");
-        CallbackData memory decoded = abi.decode(data, (CallbackData));
-        decoded.token.safeTransferFrom(decoded.payer, decoded.payee, decoded.amount);
     }
 }
