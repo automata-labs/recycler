d4239f6f06d05e13779e466bb0babc4c21c9824c
Tony Jin <nijynot@gmail.com>
2022-02-08 14:04:37 +0100

    chore: GPL-3.0-or-later license

--- src/interfaces/ICallback.sol
+++ src/interfaces/ICallback.sol
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 interface ICallback {
-    function loadCallback(bytes calldata data) external;
-
+    /// @notice Callback function for when calling the `mint` function.
+    /// @dev The target contract will have conditions for passing, the the callback is expected to
+    /// fulfill those conditions, otherwise the call stack should fail.
     function mintCallback(bytes calldata data) external;
-
-    function burnCallback(bytes calldata data) external;
 }
