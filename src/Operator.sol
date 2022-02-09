0ab3541226f42a7567c815b30d1d6517c0a033af
Tony Jin <nijynot@gmail.com>
2022-02-09 12:48:02 +0100

    feat: add management fee to operator

--- src/Operator.sol
+++ src/Operator.sol
@@ -10,11 +10,20 @@ import "./interfaces/external/ITokeVotePool.sol";
 import "./interfaces/IRecycler.sol";
 
 contract Operator is Auth {
+    /// @notice Emitted when the fee to is set.
+    event SetFeeTo(address feeTo);
+    /// @notice Emitted when the fee is set.
+    event SetFee(uint256 fee);
     /// @notice Emitted when a reactor key is set.
     event SetReactorKey(bytes32 reactorKey, bool value);
 
+    /// @notice Throws when the fee is set too high.
+    error InvalidFee();
     /// @notice Throws when voting on an invalid reactor key.
     error InvalidReactorKey(bytes32 reactorKey);
+
+    /// @notice The max fee that can be set.
+    uint256 internal constant MAX_FEE = 1e4;
 
     /// @notice The core reactor contract that holds the assets.
     address public immutable recycler;
@@ -26,6 +35,10 @@ contract Operator is Auth {
     address public immutable onchainvote;
     /// @notice The Tokemak rewards contract.
     address public immutable rewards;
+    /// @notice The recipient of the management fee.
+    address public feeTo;
+    /// @notice The management fee.
+    uint256 public fee;
 
     /// @notice Accepted reactor keys that this contract can vote with.
     mapping(bytes32 => bool) public reactorKeys;
@@ -42,9 +55,36 @@ contract Operator is Auth {
         derivative = derivative_;
         onchainvote = onchainvote_;
         rewards = rewards_;
+
+        fee = 100; // 1%
     }
 
-    function setReactorKey(bytes32 reactorKey, bool value) external {
+    /// @notice Sets the management fee to.
+    function setFeeTo(address feeTo_)
+        external
+        auth
+    {
+        feeTo = feeTo_;
+        emit SetFeeTo(feeTo);
+    }
+
+    /// @notice Sets the management fee.
+    function setFee(uint256 fee_)
+        external
+        auth
+    {
+        if (fee_ > MAX_FEE)
+            revert InvalidFee();
+
+        fee = fee_;
+        emit SetFee(fee_);
+    }
+
+    /// @notice Sets a reactor key as vaild/invalid.
+    function setReactorKey(bytes32 reactorKey, bool value)
+        external
+        auth
+    {
         reactorKeys[reactorKey] = value;
     }
 
@@ -76,28 +116,39 @@ contract Operator is Auth {
         public
         auth
     {
-        uint256 claimable = IRewards(rewards).getClaimableAmount(Recipient({
-            chainId: recipient.chainId,
-            cycle: recipient.cycle,
-            wallet: recipient.wallet,
-            amount: recipient.amount
-        }));
-
-        claim(recipient, v, r, s);
-        deposit(claimable);
+        (uint256 claimed, ) = claim(recipient, v, r, s);
+        deposit(claimed);
     }
 
     /// @notice Deposit TOKE for Recycler.
     function claim(Recipient memory recipient, uint8 v, bytes32 r, bytes32 s)
         public
         auth
+        returns (uint256 claimed, uint256 fees)
     {
+        claimed = IRewards(rewards).getClaimableAmount(Recipient({
+            chainId: recipient.chainId,
+            cycle: recipient.cycle,
+            wallet: recipient.wallet,
+            amount: recipient.amount
+        }));
+
         address[] memory targets = new address[](1);
         bytes[] memory datas = new bytes[](1);
         targets[0] = rewards;
         datas[0] = abi.encodeWithSelector(IRewards.claim.selector, recipient, v, r, s);
-
         IRecycler(recycler).execute(targets, datas);
+
+        if (fee > 0 && feeTo != address(0)) {
+            fees = (claimed * fee) / MAX_FEE;
+            claimed -= fees;
+
+            targets = new address[](1);
+            datas = new bytes[](1);
+            targets[0] = underlying;
+            datas[0] = abi.encodeWithSelector(IERC20.transfer.selector, feeTo, fees);
+            IRecycler(recycler).execute(targets, datas);
+        }
     }
 
     /// @notice Deposit TOKE for tTOKE for the Recycler.
