aef03b5706bce83bb1792817e39677869fa8bc8a
nijynot <nijynot@gmail.com>
2022-02-13 13:48:30 +0100

    feat: add destroy fn to pause (#15)
    * docs: natspec for pause

--- src/libraries/Pause.sol
+++ src/libraries/Pause.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.0;
 import "./Auth.sol";
 
 contract Pause is Auth {
+    /// @notice Emitted when a function is destroyed (paused forever).
+    /// @param sig The function signature.
+    event Destroyed(bytes4 sig);
     /// @notice Emitted when a function is paused.
+    /// @param sig The function signature.
     event Paused(bytes4 sig);
     /// @notice Emitted when a function is unpaused.
+    /// @param sig The function signature.
     event Unpaused(bytes4 sig);
+
+    uint256 public constant DESTROY = uint256(keccak256("Pause.destroy"));
 
     /// @dev The mapping from function sig to state.
     mapping(bytes4 => uint256) public paused;
@@ -22,8 +29,16 @@ contract Pause is Auth {
     /// @notice Unauthorize an address.
     /// @dev Can only be called by already authorized contracts.
     function unpause(bytes4 sig) external virtual auth {
+        require(paused[sig] != DESTROY, "Destroyed");
         paused[sig] = 0;
         emit Unpaused(sig);
+    }
+
+    /// @notice Pause a function forever.
+    /// @dev This action cannot be undone. Use with caution.
+    function destroy(bytes4 sig) external virtual auth {
+        paused[sig] = DESTROY;
+        emit Destroyed(sig);
     }
 
     modifier playback {
